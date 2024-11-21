d627c5c714d62415a6e50da049b152ef6defe49a
Paloma Eleuterio <php.eleuterio@gmail.com>
2024-11-21 16:37:33 -0300

    Cria script para chamar as correlações

--- R/get_correlation.R
+++ R/get_correlation.R
@@ -1,9 +1,10 @@
-source("R/get_parameter.R", echo = FALSE)
-source("R/correlation.R", echo = FALSE) 
+source("R/get_parameter.R")
+source("R/correlation.R") 
 
 
 estatisticas_descritivas()  
 correlacao_parametros(parametro_escolhido)  
+
 
 
 
