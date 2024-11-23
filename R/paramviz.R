42df84e2850065f76863c1c048265d3050f789bd
Paloma Eleuterio <php.eleuterio@gmail.com>
2024-11-23 01:32:08 -0300

    Cria script para gerar os datasets e depois plotar os gráficos

---
+++ R/paramviz.R
@@ -0,0 +1,10 @@
+source("R/process_air_quality_parameters.R")
+source("R/plots.R")
+
+
+plot_all_parameters <- function(dfs) {
+  walk2(names(dfs), dfs, ~plot_monthly_values(.y, .x))
+}
+
+dfs <- process_parameters(lines)
+plot_all_parameters(dfs)
